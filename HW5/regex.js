fbd8a5a70cf74c9799bb972f2372798dc37b6ad0
misbiheyv <mishagorkovenko1103@gmail.com>
2022-10-21 22:25:03 +0300

    Положил сюда файлы из 5 дз и разбил по папкам

---
+++ HW5/regex.js
@@ -0,0 +1,27 @@
+// 1 задание
+export const isLatinNumericOrDollar = (str) => /^[a-z\p{Number}$_]+$/ui.test(str);
+
+// 2 задание
+'foo    bla.bar,gd;4'
+export const split = (str) => str.split(/ +|\.|,|;/g);
+
+// 3 задание
+export function format(str, replacer) {
+    return str.replace(/\${(?<key>\w+?)}/g, (...args) => {
+        return replacer[args[args.length - 1].key]
+    })
+}
+
+// 4 задание
+
+export function normalize(str) {
+    return str.replace(/(\w{1,3}?)\1+/g, '$1')
+}
+
+// 5 задание
+
+export function calc(expression) {
+    return expression.replace(/[0-9(][0-9\+\-\*\/\(\) ]+/mig, (...args) => 
+        Function('', `return ${args[0]}`)()
+    )
+}
---
+++ HW5/regex.spec.js
@@ -0,0 +1,36 @@
+import * as regex from './regex';
+
+const str = `
+Какой-то текст (10 + 15 - 24) ** 2
+Еще какой-то текст 2 * 10
+`;
+
+const calculatedStr = `
+Какой-то текст 1
+Еще какой-то текст 20
+`;
+
+describe('', () => {
+    test('function check the string has different symbols of latin, digits, dollar sign and slash.', () => {
+        expect(regex.isLatinNumericOrDollar('вфвфвыфвф123ы')).toBe(false)
+        expect(regex.isLatinNumericOrDollar('asd$_123')).toBe(true)
+    })
+
+    test('function split the string by symbols: "." "," ";" and spaces.', () => {
+        expect(regex.split('foo    bla.bar,gd;4')).toEqual(['foo', 'bla', 'bar', 'gd', '4'])
+    })
+
+    test('function split the string by symbols: "." "," ";" and spaces.', () => {
+        expect(regex.format('Hello, ${user}! Your age is ${age}.', {user: 'Misha', age: 20})).toBe('Hello, Misha! Your age is 20.')
+    })
+
+    test('Remove a groups of repeating 1-3 symbols.', () => {
+        expect(regex.normalize('aaaabbbbczzzz')).toBe('abcz');
+        expect(regex.normalize('abababbbabcabc')).toBe('abbabc');
+        expect(regex.normalize('foofoobabaaaazze')).toBe('foobaaze');
+    })
+
+    test('Calculate math expressions in string and replace them by result.', () => {
+        expect(regex.calc(str)).toBe(calculatedStr)
+    })
+})
